a3348969267bf2548e42910bfdebba0b917213f2
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-10-07 12:28:57 +0200

    add old-apns-conf.xml
    old-apns-conf.xml is needed in by telephony
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- device.mk
+++ device.mk
@@ -205,7 +205,9 @@ PRODUCT_PACKAGES += \
     e2fsck
 
 # APN list
-PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES += \
+    device/sample/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml \
+    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Platform specific properties
 #
