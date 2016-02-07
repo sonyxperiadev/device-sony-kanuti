c26e11c8908c1ea9af8c8be0a0e93199f30d64c2
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2016-02-07 22:00:16 +0100

    move common family stuff

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, device/sony/common/common.mk)
+
 SOMC_PLATFORM := kanuti
 
 SONY_ROOT = device/sony/kanuti/rootdir
