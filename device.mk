51fdbc654f74fdbedf4806fb387230b642974907
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2016-02-07 22:00:42 +0100

    fix inclusion order

--- device.mk
+++ device.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/sony/common/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 SOMC_PLATFORM := kanuti
 
