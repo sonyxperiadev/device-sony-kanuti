75be50761465babfbc540c6a0a775b0090fb02f9
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-06-09 13:00:54 +0200

    remove local display HAL
    we should not have an local HAL
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- display/Android.mk
+++
@@ -1,5 +0,0 @@
-display-hals := libgralloc libgenlock libcopybit libmemtrack
-display-hals += libhwcomposer liboverlay libqdutils libhdmi libqservice
-
-include $(call all-named-subdir-makefiles,$(display-hals))
-
