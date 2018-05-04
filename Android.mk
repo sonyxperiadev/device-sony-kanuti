sonyxperiadev/device-sony-loire

use TARGET_BOOTLOADER_BOARD_NAME as build barier
the PRODUCT_PLATFORM is deprecated and replaced by TARGET_BOOTLOADER_BOARD_NAME

Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

Android.mk
@@ -1,4 +1,4 @@
-ifeq (loire,$(PRODUCT_PLATFORM))
+ifeq (loire,$(TARGET_BOOTLOADER_BOARD_NAME))
 
 LOCAL_PATH := $(call my-dir)
 

sonyxperiadev/device-sony-kanuti

Android.mk
@@ -1,4 +1,4 @@
-ifeq (kanuti,$(PRODUCT_PLATFORM))
+ifeq (kanuti,$(TARGET_BOOTLOADER_BOARD_NAME))
 
 LOCAL_PATH := $(call my-dir)
 