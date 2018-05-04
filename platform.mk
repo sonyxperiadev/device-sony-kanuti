sonyxperiadev/device-sony-loire

move platform specific init.rc to vendor
for a simplified init.rc we are moving the platform
specific init to vendor/etc/init

please note that the init.common.rc will be used to boot the device
while init.<platform>.rc will contain only device specific init

Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

platform.mk
@@ -59,9 +59,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/vendor/etc/rqbalance_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/rqbalance_config.xml
 
-# Platform power configuration
+# Platform specific init
 PRODUCT_PACKAGES += \
-    init.loire.pwr
+    init.loire \
+    init.loire.pwr \
+    ueventd
 
 # Audio
 PRODUCT_PACKAGES += \

rootdir/Android.mk
@@ -10,23 +10,23 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := init.$(TARGET_DEVICE)
-LOCAL_SRC_FILES := init.loire.rc
+LOCAL_MODULE := init.loire
+LOCAL_SRC_FILES := vendor/etc/init/init.loire.rc
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_STEM := init.$(TARGET_DEVICE)
+LOCAL_MODULE_STEM := init.loire
 LOCAL_MODULE_SUFFIX := .rc
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.loire.pwr
-LOCAL_SRC_FILES := init.loire.pwr.rc
+LOCAL_SRC_FILES := vendor/etc/init/init.loire.pwr.rc
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_STEM := init.loire.pwr
 LOCAL_MODULE_SUFFIX := .rc
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

sonyxperiadev/device-sony-kanuti

platform.mk
@@ -53,9 +53,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/vendor/etc/rqbalance_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/rqbalance_config.xml
 
-# Platform power configuration
+# Platform specific init
 PRODUCT_PACKAGES += \
-    init.kanuti.pwr
+    init.kanuti \
+    init.kanuti.pwr \
+    ueventd
 
 # Audio
 PRODUCT_PACKAGES += \

rootdir/Android.mk
@@ -10,23 +10,23 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := init.$(TARGET_DEVICE)
-LOCAL_SRC_FILES := init.kanuti.rc
+LOCAL_MODULE := init.kanuti
+LOCAL_SRC_FILES := vendor/etc/init/init.kanuti.rc
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_STEM := init.$(TARGET_DEVICE)
+LOCAL_MODULE_STEM := init.loire
 LOCAL_MODULE_SUFFIX := .rc
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.kanuti.pwr
-LOCAL_SRC_FILES := init.kanuti.pwr.rc
+LOCAL_SRC_FILES := vendor/etc/init/init.kanuti.pwr.rc
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_STEM := init.kanuti.pwr
 LOCAL_MODULE_SUFFIX := .rc
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)